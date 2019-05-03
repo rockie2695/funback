e43e08ab9769f3ae1a2da07f2702c8a56847f659
rockie2695 <21993381+rockie2695@users.noreply.github.com>
2019-05-03 17:44:11 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -36,37 +36,78 @@ app.post("/insert/:collection", function (req, res) {
         res.send({ "error": "No input!" });
         return;
     } else {
-        req.body.blood=0
-        req.body.lv=0
-        req.body.mana=0
-        req.body.phy=0
-        req.body.soul=0
-        req.body.magic=0
-        req.body.time=Date.now()
+        req.body.blood = 0
+        req.body.lv = 0
+        req.body.mana = 0
+        req.body.phy = 0
+        req.body.soul = 0
+        req.body.magic = 0
+        req.body.time = Date.now()
         console.log(req.body)
         mongoConectClient.connect(err => {
             if (err) {
                 console.log(err)
+                res.send({ "error": err });
+                return;
             } else {
-                let collection = mongoConectClient.db("rockie2695_mongodb").collection("fun_"+req.params.collection);
+                let collection = mongoConectClient.db("rockie2695_mongodb").collection("fun_" + req.params.collection);
                 insert(collection,
                     req.body
                     , function (err, result) {
                         if (err) {
                             res.send({ "error": err });
+                            return;
                         } else {
-                            let sendValue=result.ops[0]
-                            sendValue._id=ObjectId(sendValue._id).toString()
+                            let sendValue = result.ops[0]
+                            sendValue._id = ObjectId(sendValue._id).toString()
                             res.send({ "ok": sendValue });
+                            return;
                         }
-                    })
+                    }
+                )
             }
         })
     }
 });
+app.post("/delete/:collection", function (req, res) {
+    if (!req.body) {
+        res.send({ "error": "No input!" });
+        return;
+    } else {
+        mongoConectClient.connect(err => {
+            if (err) {
+                console.log(err)
+                res.send({ "error": err });
+            } else {
+                let collection = mongoConectClient.db("rockie2695_mongodb").collection("fun_" + req.params.collection);
+                let sendValue=req.body
+                sendValue._id = ObjectId(sendValue._id)
+                deleteRecord(collection,
+                    sendValue
+                    , function (err, result) {
+                        if (err) {
+                            res.send({ "error": err });
+                            return;
+                        } else {
+                            let sendValue = result.deletedCount
+                            res.send({ "ok": sendValue });
+                            return;
+                        }
+                    }
+                )
+            }
 
+
+        })
+    }
+});
 function insert(collection, query, callback) {
     collection.insertOne(query, function (err, result) {
+        callback(err, result);
+    });
+}
+function deleteRecord(collection, query, callback) {
+    collection.deleteOne(query, function (err, result) {
         callback(err, result);
     });
 }
